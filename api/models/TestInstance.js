a8f573b3adb9be893af0248224a13e8ab06bcbe6
prorunner <diogoviannaaraujo@gmail.com>
2015-05-28 22:02:42 -0400

    Created Test Service placeholder

--- api/models/TestInstance.js
+++ api/models/TestInstance.js
@@ -10,11 +10,13 @@ module.exports = {
   attributes: {
     state: { type: 'string', defaultsTo: 'init' },
     env: { type: 'string', required: 'true' },
+    source: { type: 'json' },
     result: { type: 'json' }
   },
 
   afterCreate: function(values, cb) {
     console.log(values);
+    //testetService.startTest(values.id, values.env, values.source);
     cb()
   }
 
