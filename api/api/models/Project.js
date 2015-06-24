f949caebf631523d33d244ee17da3c371f265c68
joseph <jardix22@gmail.com>
2015-06-23 22:07:41 -0500

    Minor bug resolved

--- api/api/models/Project.js
+++ api/api/models/Project.js
@@ -10,7 +10,7 @@ module.exports = {
   attributes: {
     name: { type: 'string', required: true },
     owner: { model: 'User' },
-    envs: { collection: 'Env', via: 'projetc' },
+    envs: { collection: 'Env', via: 'project' },
     repo: { type: 'string' },
     testInstances: { collection: 'TestInstance' },
     key: { type: 'string' }
