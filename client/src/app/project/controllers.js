6d72f803f0538d86e91f47665487260add677338
Joseph Ramos <joseph.ramos@taller.net.br>
2015-07-05 14:46:03 -0500

    Create project module.

--- client/src/app/project/controllers.js
+++ client/src/app/project/controllers.js
@@ -1,2 +1,56 @@
+'use strict';
+
 angular.module('prorunner.project')
-  .controller('ProjectCtrl', ['$scope', function($scope) {}]);
+
+  .controller('ProjectListCtrl', [
+    '$scope',
+    '$sailsSocket',
+    function($scope, $sailsSocket) {
+      // Get projects
+      $sailsSocket.get('/project')
+        .success(function (data, status, headers, config) {
+          $scope.items = data;
+        });
+
+      // Remove item
+      $scope.cofirmDelete = function (index) {
+        var projectId = $scope.items[index].id;
+
+        $sailsSocket.delete('/project/' + projectId)
+        .success(function (data, status, headers, config) {
+          $scope.items.splice(index, 1);
+        });
+      }
+    }
+  ])
+
+  .controller('ProjectCtrl', [
+    '$scope', '$state',
+    '$sailsSocket',
+    function($scope, $state, $sailsSocket) {
+      var itemId = $state.params.id;
+      // Get projects
+      $sailsSocket.get('/project/' + itemId)
+        .success(function (data, status, headers, config) {
+          $scope.item = data;
+        });
+    }
+  ])
+
+  .controller('ProjectAddCtrl', [
+    '$scope', '$state', '$sailsSocket',
+    function($scope, $state, $sailsSocket) {
+
+      $scope.project = {
+        name: '',
+        description: ''
+      };
+
+      $scope.createProject = function () {
+        $sailsSocket.post('/project', $scope.project)
+        .success(function (data, status, headers, config) {
+          $state.go('projects');
+        });
+      };
+    }
+  ]);
--- client/src/app/project/module.js
+++ client/src/app/project/module.js
@@ -1,1 +1,38 @@
-angular.module('prorunner.project', []);
+'use strict';
+
+angular.module('prorunner.project', [])
+  .config([
+    '$stateProvider',
+    function ($stateProvider) {
+      $stateProvider
+        .state('projects', {
+          url: '/projects',
+          views: {
+            'content@': {
+              templateUrl: '/prorunner/project/templates/list.html',
+              controller: 'ProjectListCtrl'
+            }
+          }
+        })
+
+        .state('projects.project', {
+          url: '/{id:[0-9]{1,4}}',
+          views: {
+            'content@': {
+              templateUrl: '/prorunner/project/templates/project.html',
+              controller: 'ProjectCtrl'
+            }
+          }
+        })
+
+        .state('projects.create', {
+          url: '/create',
+          views: {
+            'content@': {
+              templateUrl: '/prorunner/project/templates/create.html',
+              controller: 'ProjectAddCtrl'
+            }
+          }
+        });
+    }
+  ]);
